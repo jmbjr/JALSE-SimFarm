a46c8f7ebd92686df5ccdcf4c874ab23e1021955
John Boyle <boylejm@gmail.com>
2017-04-15 13:11:24 -0700

    only allow Child and Adult set size. prevents Rester.class from changing size on death

--- src/jmbjr/simland/entities/TransformationListener.java
+++ src/jmbjr/simland/entities/TransformationListener.java
@@ -12,9 +12,14 @@ public class TransformationListener implements EntityTypeListener {
 	final Animal animal = event.getEntity().asType(Animal.class);
 	final Class<? extends Entity> type = event.getTypeChange();
 
+	//need to adjust stats based on makeup of Animal
+	//i.e. size is based on maturity for now.
+	
 	animal.setSightRange(FarmAnimalProperties.getSightRange(type));
 	animal.setSpeed(FarmAnimalProperties.getSpeed(type));
 	animal.setHealth(FarmAnimalProperties.getHealth(type));
-	animal.setSize(FarmAnimalProperties.getSize(type));
+	if (type.equals(Adult.class) || type.equals(Child.class)) { //maybe add a new type Alive.class ? or something else to bucket these things so Rester and Grazer don't need defined
+		animal.setSize(FarmAnimalProperties.getSize(type));
+	}
     }
 }
